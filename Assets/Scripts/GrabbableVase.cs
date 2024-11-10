8a410f8eca491a33629ba440aa4a2b972253b06f
Gabrian Chua <111519944+gchuaunc@users.noreply.github.com>
2024-11-09 20:51:02 -0500

    feat(wip): add vase puzzle

---
+++ Assets/Scripts/GrabbableVase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class GrabbableVase : RayTappableObject
+{
+
+    protected override void TriggerAction()
+    {
+        // nothing, we only care about it being placed on the correct pedestal
+    }
+}
